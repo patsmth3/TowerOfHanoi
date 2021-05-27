08460557ec425d2d4f17e9e774805d0b491afaf9
Pat Smith <44172380+patsmth3@users.noreply.github.com>
2021-05-27 13:02:32 -0400

    Add files via upload

---
+++ TowerOfHanoi/TowerOfHanoi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    /// <summary>
+    /// This game tells you the moves that should be taken when playing Tower Of Hanoi
+    /// </summary>
+    
+    class Program
+    {
+       static int counter = 0;
+        static void Main(string[] args)
+        {
+            var discs = 0;
+            
+            Console.WriteLine("Please enter the number of disc: ");
+            
+            discs = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            Tower(discs, 1, 3, 2);
+        }
+        static void Tower(int n, int sourcePeg, int destinationPeg, int sparePeg)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(counter + " " + sourcePeg + " ->" + destinationPeg);
+                counter++;
+            }
+            else
+            {
+                Tower(n - 1, sourcePeg, sparePeg, destinationPeg);
+
+                Console.WriteLine(counter + " " + sourcePeg + "->" + destinationPeg);
+                counter++;
+                
+                Tower(n - 1, sparePeg, destinationPeg, sourcePeg);
+            }
+        }
+    }
+}
+	
